2714722d4567271365e1f2e0a6a21f8850978772
TymurTkachenko <tymur.tk@gmail.com>
2020-04-16 01:40:06 -0400

    finds options withing price range

--- main.py
+++ main.py
@@ -10,14 +10,16 @@ with open("secrets.json") as json_file:
     token = json.load(json_file)["auth"]
 
 HEADERS = {'Authorization': f'Bearer {token}', 'Accept': 'application/json'}
+APISERVER = "https://sandbox.tradier.com" # Change here to use a different API
+STRIKERANGE = 5 # Change here to get a larger or smaller range of options by their distance to the current price
 
 def request(symbol: str, date: str):
-    #options_chains = request_options_chains(symbol, next_friday(last_year(date)))
-    call1 = put(symbol, date)
+    request_options_chains(symbol, date)
+    call1 = call(symbol, date)
     print(call1)
 
 def request_history(symbol: str, start: str, end: str):
-    response = requests.get(f'https://sandbox.tradier.com/v1/markets/history', params={'symbol': symbol, 'start': start, 'end': end}, headers=HEADERS)
+    response = requests.get(f'{APISERVER}/v1/markets/history', params={'symbol': symbol, 'start': start, 'end': end}, headers=HEADERS)
 
     history = response.json()
     with open("history.json", "w") as write_file:
@@ -26,7 +28,11 @@ def request_history(symbol: str, start: str, end: str):
     return history
 
 def request_options_chains(symbol: str, expiration: str):
-    response = requests.get(f'https://sandbox.tradier.com/v1/markets/options/chains', params={'symbol': symbol, "expiration": expiration}, headers=HEADERS)
+    
+    expiration = datetime.strptime(expiration, "%Y-%m-%d")
+    expiration = get_friday_last_year(expiration).strftime("%y%m%d")
+    
+    response = requests.get(f'{APISERVER}/v1/markets/options/chains', params={'symbol': symbol, "expiration": expiration}, headers=HEADERS)
 
     options_chains = response.json()
     with open("options_chains.json", "w") as write_file:
@@ -34,40 +40,63 @@ def request_options_chains(symbol: str, expiration: str):
 
     return options_chains
 
-def request_chain(symbol: str):
-    response = requests.get(f'https://sandbox.tradier.com/v1/markets/history', params={'symbol': symbol}, headers=HEADERS)
+def request_chain(symbol: str, start: str):
+    response = requests.get(f'{APISERVER}/v1/markets/history', params={'symbol': symbol, "start": start}, headers=HEADERS)
 
     chain = response.json()
-    with open("chain.json", "w") as write_file:
-        json.dump(chain, write_file, indent=4, sort_keys=True)
     
     return chain
 
 def get_day_price(symbol: str, date):
     
-    history = request_history(symbol, three_weeks_ago(datetime.strptime(date, '%Y-%m-%d')), three_weeks_ahead(datetime.strptime(date, '%Y-%m-%d')))
+    date = f"{date[:2]}-{date[2:4]}-{date[4:6]}"
+    history = request_history(symbol, datetime.strptime(date, '%y-%m-%d'), (datetime.strptime(date, '%y-%m-%d')) + timedelta(days=1))
 
-    for day in history["history"]["day"]:
-        if day["date"] == date:
-            price = day["high"]
-            return price
+    return history["history"]["day"]["high"]
 
 def call(symbol: str, date: str):
+    
+    chain = []
+
     day = datetime.strptime(date, "%Y-%m-%d")
     day = get_friday_last_year(day).strftime("%y%m%d")
-    chain = request_chain(f"{symbol}{day[0:2]}{day[2:4]}{day[4:6]}C{math.ceil(get_day_price(symbol, date)):05d}000")
+
+    price = math.floor(get_day_price(symbol, day))
+    price_range = get_price_range(price - STRIKERANGE, price + STRIKERANGE)
+
+    for price in price_range:
+        chain.append(request_chain(f"{symbol}{day[0:2]}{day[2:4]}{day[4:6]}C{price:05d}000", datetime.strptime(day, "%y%m%d") - timedelta(days=5)))
+    
+    with open("chain.json", "w") as write_file:
+        json.dump(chain, write_file, indent=4, sort_keys=True)
+
     return chain
 
 def put(symbol: str, date: str):
+    
+    chain = []
+
     day = datetime.strptime(date, "%Y-%m-%d")
     day = get_friday_last_year(day).strftime("%y%m%d")
-    #chain = request_chain(f"{symbol}{day[0:2]}{day[2:4]}{day[4:6]}P{math.ceil(get_day_price(symbol, date)):05d}000")
-    chain = request_chain("VZ200417P00056000")
+
+    price = math.floor(get_day_price(symbol, day))
+    price_range = get_price_range(price - STRIKERANGE, price + STRIKERANGE)
+
+    for price in price_range:
+        chain.append(request_chain(f"{symbol}{day[0:2]}{day[2:4]}{day[4:6]}P{price:05d}000", datetime.strptime(day, "%y%m%d") - timedelta(days=5)))
+    
+    with open("put.json", "w") as write_file:
+        json.dump(chain, write_file, indent=4, sort_keys=True)
+        
     return chain
 
 def third_fridays(d, n):
- 
+    """Given a date, calculates n next third fridays
+    https://stackoverflow.com/questions/28680896/how-can-i-get-the-3rd-friday-of-a-month-in-python/28681097"""
+
     def next_third_friday(d):
+        """Given a third friday find next third friday"""
+
         d += timedelta(weeks=4)
         return d if d.day >= 15 else d + timedelta(weeks=1)
 
@@ -83,20 +112,35 @@ def third_fridays(d, n):
     return result[0]
 
 def get_friday_last_year(d):
+    """Given a date, d, finds the next friday of the date 365 days prior"""
+    
     d = last_year(d)
     d = next_friday(d)
     return d
 
 def three_weeks_ago(d):
+    """Given a date, d, finds the date 3 weeks prior"""
+    
     return d - timedelta(days=21)
 
 def three_weeks_ahead(d):
+    """Given a date, d, finds the date in 3 weeks"""
+    
     return d + timedelta(days=21)
 
 def last_year(d):
+    """Given a date, d, finds the date 365 days prior"""
+    
     return d - timedelta(days=365)
 
 def next_friday(d):
+    """Given a date, d, finds the date of the next friday"""
+    
     return d + timedelta((4-d.weekday()) % 7)
 
-request("GOOGL", "2020-01-17")
+def get_price_range(start: int, end: int):
+    """Returns a list of the numbers start to end, inclusive"""
+
+    return list(range(start, end + 1))
+
+request("VZ", "2020-04-03")
